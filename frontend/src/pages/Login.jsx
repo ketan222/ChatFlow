ceb0f758753cc288f9b01e1054ac741be3f57620
Ketan222 <ketanmodgil@gmail.com>
2025-08-25 22:10:33 +0530

    started project

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <div className="w-screen h-screen bg-amber-300 box-border">
+      <div className="border-gray-400">Login</div>
+    </div>
+  );
+}
